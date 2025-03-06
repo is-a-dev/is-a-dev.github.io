959da37470839d0d3c87c02605fed7e365b2ffbc
William Harrison <87287585+wdhdev@users.noreply.github.com>
2025-03-06 19:17:44 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,122 +1,112 @@
-let is_searching=false;
-let pattern_domain='^[a-zA-Z0-9_][a-zA-Z0-9.-]*';
-let timer_search=undefined;
-let reserved_domain=undefined;
-const search_domain=document.getElementById('search-domain');
-// result containers
-const result_card=document.getElementById('result-card');
-const result_none=document.getElementById('result-none');
-const result_error=document.getElementById('result-error');
-const result_reserved=document.getElementById('result-reserved');
-// user propagate
-const user_avatar=document.getElementById('user-avatar');
-const user_gh_link=document.getElementById('user-gh-link');
-const user_name=document.getElementById('user-name');
-const user_website_link=document.getElementById('user-website-link');
-const user_info=document.getElementById('user-info');
+let is_searching = false;
+let pattern_domain = "^[a-zA-Z0-9_][a-zA-Z0-9.-]*";
+let timer_search = undefined;
+let reserved_domain = undefined;
+const search_domain = document.getElementById("search-domain");
+const result_card = document.getElementById("result-card");
+const result_none = document.getElementById("result-none");
+const result_error = document.getElementById("result-error");
+const result_reserved = document.getElementById("result-reserved");
+const user_avatar = document.getElementById("user-avatar");
+const user_gh_link = document.getElementById("user-gh");
+const user_gh_name = document.getElementById("user-gh-name");
 
-function check(){
-    const search= search_domain.value.trim().replace(/\s/g, '-').toLowerCase();
-    if(isReserved(search)){
+function check() {
+    const search = search_domain.value.trim().replace(/\s/g, "-").toLowerCase();
+    if (isReserved(search)) {
         hideResults();
-        result_reserved.classList.toggle('hidden');
+        result_reserved.classList.toggle("hidden");
         return;
     }
-    if(!search || is_searching) return;
-    is_searching=true;
+    if (!search || is_searching) return;
+    is_searching = true;
     fetch(`https://raw.githubusercontent.com/is-a-dev/register/main/domains/${search}.json`)
-    .then(response => {
-        hideResults();
-        if(!response.ok && response.status !== 404)
-            throw new Error('Something went wrong');
-        return !response.ok? null :
-                            response.json();
-    })
-    .then(json => {
-        if(json)
-        {
-            json.search=search;
-            propagateResult(json);
-        }
-        else if (result_none.classList.contains('hidden')) 
-            result_none.classList.toggle('hidden');
-    })
-    .catch((reason)=>{
-        console.log(reason);
-        if (result_error.classList.contains('hidden')) result_error.classList.toggle('hidden');
-    }).finally(()=>{
-        is_searching=false;
-    });
+        .then((response) => {
+            hideResults();
+            if (!response.ok && response.status !== 404) throw new Error("Something went wrong");
+            return !response.ok ? null : response.json();
+        })
+        .then((json) => {
+            if (json) {
+                json.search = search;
+                propagateResult(json);
+            } else if (result_none.classList.contains("hidden")) result_none.classList.toggle("hidden");
+        })
+        .catch((reason) => {
+            console.log(reason);
+            if (result_error.classList.contains("hidden")) result_error.classList.toggle("hidden");
+        })
+        .finally(() => {
+            is_searching = false;
+        });
 }
 
-function propagateResult(data){
-    if (result_card.classList.contains('hidden')) result_card.classList.remove('hidden');
-    var gh_link=`https://github.com/${data.owner.username}`;
-    var website_link=`${data.search}.is-a.dev`;
-    user_avatar.src=`${gh_link}.png?size=120`;
-    user_gh_link.href=gh_link;
-    user_name.innerHTML=data.owner.username;
-    user_website_link.href=`https://${website_link}`;
-    user_website_link.innerHTML=website_link;
-    user_info.textContent= data.description || '';
+function propagateResult(data) {
+    if (result_card.classList.contains("hidden")) result_card.classList.remove("hidden");
+    var gh_link = `https://github.com/${data.owner.username}`;
+    user_avatar.src = `${gh_link}.png?size=64`;
+    user_gh_link.href = gh_link;
+    user_gh_name.innerHTML = data.owner.username;
 }
 
-function hideResults(){
-        if (!result_card.classList.contains('hidden')) result_card.classList.toggle('hidden');
-        if (!result_none.classList.contains('hidden')) result_none.classList.toggle('hidden');
-        if (!result_error.classList.contains('hidden')) result_error.classList.toggle('hidden');
-        if (!result_reserved.classList.contains('hidden')) result_reserved.classList.toggle('hidden');
+function hideResults() {
+    if (!result_card.classList.contains("hidden")) result_card.classList.toggle("hidden");
+    if (!result_none.classList.contains("hidden")) result_none.classList.toggle("hidden");
+    if (!result_error.classList.contains("hidden")) result_error.classList.toggle("hidden");
+    if (!result_reserved.classList.contains("hidden")) result_reserved.classList.toggle("hidden");
 }
 
-function isReserved(domain){
+function isReserved(domain) {
     const reserved_domains = new Set(reserved_domain);
-    return reserved_domains.has(domain) || reserved_domain.some(pattern => pattern.includes('[') && new RegExp(pattern).test(domain));
+    return (
+        reserved_domains.has(domain) ||
+        reserved_domain.some((pattern) => pattern.includes("[") && new RegExp(pattern).test(domain))
+    );
 }
 
 async function fetchReservedDomains() {
     try {
-      if (typeof reserved_domain === 'undefined') {
-        if (typeof timer_search !== 'undefined') {
-          clearInterval(timer_search);
-        }
-  
-        const response = await fetch('https://raw.githubusercontent.com/is-a-dev/register/main/util/reserved.json');
-        if (!response.ok) {
-          throw new Error('Something went wrong');
-        }
-  
-        reserved_domain = await response.json();
-        search_domain.disabled = false;
-        search_domain.placeholder = 'e.g. william';
-        document.getElementById('search-keyword').innerHTML = '*Start typing to check the subdomain availability, <a alt="filname-convention" target="_blank" href="https://docs.is-a.dev/domain-structure/#filename">check here</a> to learn how to create a proper domain name.';
-      }
-    } catch (error) {
-      console.error('Error fetching reserved domains:', error);
-    }
-  }
+        if (typeof reserved_domain === "undefined") {
+            if (typeof timer_search !== "undefined") {
+                clearInterval(timer_search);
+            }
 
-  search_domain.addEventListener('input', function(evt) {
+            const response = await fetch("https://raw.githubusercontent.com/is-a-dev/register/main/util/reserved.json");
+            if (!response.ok) {
+                throw new Error("Something went wrong");
+            }
+
+            reserved_domain = await response.json();
+            search_domain.disabled = false;
+            search_domain.placeholder = "e.g. william";
+        }
+    } catch (error) {
+        console.error("Error fetching reserved domains:", error);
+    }
+}
+
+search_domain.addEventListener("input", function (evt) {
     const input_value = this.value.trim();
 
-    if (input_value === '') {
-      clearTimer();
-      hideResults();
-      return;
+    if (input_value === "") {
+        clearTimer();
+        hideResults();
+        return;
     }
-  
+
     const matched_value = input_value.match(new RegExp(pattern_domain));
     if (matched_value) {
-      search_domain.value = matched_value[0];
+        search_domain.value = matched_value[0];
     }
-  
+
     clearTimer();
     timer_search = setTimeout(() => check(), 1000);
-  });
-  
-  function clearTimer() {
+});
+
+function clearTimer() {
     if (timer_search !== undefined) {
-      clearInterval(timer_search);
+        clearInterval(timer_search);
     }
-  }
+}
 
 fetchReservedDomains();
